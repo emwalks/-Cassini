dc1995dd8370118ec336f672bd22755e88d2a3e7
Emma Walker <walkere@tcd.ie>
2020-03-03 18:13:22 +0000

    moves the nav controller check for detail view to an extension

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -9,28 +9,33 @@
 import UIKit
 
 class CassiniViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if let url = DemoURLs.NASA[identifier]{
-                var destination = segue.destination
-                // this unwraps the nav controller and gets its visible vc
-                // it will still work if there wasn't a nav con
-                if let navcon = destination as? UINavigationController{
-                    destination = navcon.visibleViewController ?? navcon
-                }
-                if let imageVC = destination as? ImageViewController {
+                if let imageVC = segue.destination.contents as? ImageViewController {
                     imageVC.imageURL = url
                     imageVC.title = (sender as? UIButton)?.currentTitle
                 }
             }
         }
     }
+}
 
+extension UIViewController {
+    var contents: UIViewController {
+        // this unwraps the nav controller and gets its visible vc
+        // it will still work if there wasn't a nav con
+        if let navcon = self as? UINavigationController{
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
 }
